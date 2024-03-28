1dd93c50dae1ebb79faa27cdcd6d9ad174ef896f
Victor Winberg <victor.m.winberg@gmail.com>
2024-03-28 15:44:30 +0100

    feat: custom git tag versioning

--- src/git.ts
+++ src/git.ts
@@ -7,7 +7,7 @@ const { workspace = "" } = config.bitbucket;
 const dir = `${__dirname}/../dump`;
 
 const cmd = (dir?: string) => {
-  return $({ stdio: "inherit", cwd: dir });
+  return $({ stdout: ["inherit", "pipe"], cwd: dir });
 };
 
 const args = process.argv.slice(2);
@@ -26,11 +26,22 @@ switch (command) {
     await cmd(dir)`git clone git@bitbucket.org:${workspace}/${repo}.git`;
 
     if (repo === "august-model") {
-      await cmd(`${dir}/${repo}`)`uglyrelease ${extra[0]}`;
+      // Custom versioning
+      await cmd(`${dir}/${repo}`)`npm version ${extra[0]} --no-git-tag-version`;
+
+      const v = "require('./package.json').version";
+      const { stdout: version } = await cmd(`${dir}/${repo}`)`node -p ${v}`;
+
+      const msg = `[skip ci] ${version}`;
+      await cmd(`${dir}/${repo}`)`git commit -am ${msg}`;
+
+      const tag = `source tag: ${version}-src`;
+      await cmd(`${dir}/${repo}`)`git tag -am ${tag} v${version}-src`;
     } else {
       await cmd(`${dir}/${repo}`)`npm version ${extra[0]}`;
-      await cmd(`${dir}/${repo}`)`git push --follow-tags`;
     }
+
+    await cmd(`${dir}/${repo}`)`git push --follow-tags`;
     await cmd(dir)`rm -rf ${repo}`;
     break;
 }
